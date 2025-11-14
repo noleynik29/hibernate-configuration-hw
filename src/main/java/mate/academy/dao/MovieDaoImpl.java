694474176529bebeb4966a4d2db2649f0343ad48
noleynik29 <noleynik29@gmail.com>
2025-11-14 14:18:28 +0200

    Done task

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -2,16 +2,18 @@ package mate.academy.dao;
 
 import java.util.Optional;
 import mate.academy.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
-    private final SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory = HibernateUtil.getInstance();
 
-    public MovieDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public MovieDaoImpl() {
     }
 
     @Override
